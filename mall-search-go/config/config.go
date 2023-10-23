b0437289ff69c07ec047d9aadae9ba288d70f16a
Lavzzz <2513277606@qq.com>
2023-10-23 23:21:14 +0800

    update Dsn

--- mall-search-go/config/config.go
+++ mall-search-go/config/config.go
@@ -1,5 +1,5 @@
 package config
 
 const (
-	Dsn = "root:root@tcp(localhost:3306)/mall?charset=utf8mb4&parseTime=True&loc=Local"
+	Dsn = "root:root@tcp(127.0.0.1:3306)/mall?charset=utf8mb4&parseTime=True&loc=Local"
 )
